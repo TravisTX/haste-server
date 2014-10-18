d3c71d59b0b6d15cacf71c769cc028ef9f5e74b1
TravisTX <travistx@gmail.com>
2014-10-18 16:45:13 -0400

    moved paste route to the root

--- static/app/routes.js
+++ static/app/routes.js
@@ -3,30 +3,16 @@
 pasteApp.constant('routes', {
     routes: [
         {
-            name: 'base',
-            url: '/',
-            controller: 'BaseController',
-            templateUrl: '/app/base/base.html'
-        },
-        {
             name: 'paste',
-            parent: 'base',
-            url: 'paste/{pasteKey}/{format}',
+            url: '/{pasteKey}/{format}',
             controller: 'PasteController',
             templateUrl: '/app/paste/paste.html'
         },
         {
             name: 'paste_new',
-            parent: 'base',
-            url: 'paste',
+            url: '/',
             controller: 'PasteController',
             templateUrl: '/app/paste/paste.html'
-        },
-        {
-            name: 'raw',
-            parent: 'base',
-            url: 'raw/{pastetId}',
-            templateUrl: '/app/raw/raw.html'
         }
     ],
 
